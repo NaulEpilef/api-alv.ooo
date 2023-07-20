be40d1576f222f09ce7055d419d9eed9dc0fc3b6
Luan Felipe <lfps1999@gmail.com>
2023-07-19 21:06:56 -0300

    IErrorResponse now returns message inside data

--- src/interfaces/error.ts
+++ src/interfaces/error.ts
@@ -1,5 +1,5 @@
 export type ISuccessResponse<T> = { status: "success", data: T };
-export type IErrorResponse = { status: "error", message: string };
+export type IErrorResponse = { status: "error", data: { message: string } };
 
 export type IApiResponse<T> = 
   | ISuccessResponse<T>
