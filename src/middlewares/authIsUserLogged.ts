e8c8dadaf5bff823662d0731c5117503a90d05af
Luan Felipe <lfps1999@gmail.com>
2023-07-22 00:28:20 -0300

    Temporary validation if token is null string

--- src/middlewares/authIsUserLogged.ts
+++ src/middlewares/authIsUserLogged.ts
@@ -6,10 +6,10 @@ const prisma = new PrismaClient();
 
 const authIsUserLogged = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1]
+    const token = req.headers.authorization?.split(' ')[1];
     req.body.isLogged = true;
 
-    if (!token) {
+    if (!token || token == "null") {
       req.body.isLogged = false;
       next();
       return;
